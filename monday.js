94ad399c282e2201e995865d3e56140dd35a9db8
Axyl S <axylschulz0@gmail.com>
2025-07-27 11:13:05 -0600

    Add Monday Go study schedule

--- monday.js
+++ monday.js
@@ -1,3 +1,6 @@
 export default [
+  "Tsumego warm-up (15-20 min): basic shapes",
+  "Read fundamentals chapter: opening principles or tesuji",
+  "Easy tesuji set (15 min): quantity over difficulty",
   "Pimsleur lesson"
 ];
