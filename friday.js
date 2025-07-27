3bb393579d9dbfb4f90f98d9f25fa15cec723292
Axyl S <axylschulz0@gmail.com>
2025-07-27 11:16:52 -0600

    Add Go review routine to Friday

--- friday.js
+++ friday.js
@@ -1,3 +1,6 @@
 export default [
-  "Pimsleur lesson"
+  "Pimsleur lesson",
+  "Tsumego (20m) - try intermediate tesuji puzzles",
+  "Pro game review (45m) - focus on fights and defense",
+  "Own game review (15m) - apply pro ideas"
 ];
